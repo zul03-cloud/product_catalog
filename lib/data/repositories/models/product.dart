44054d7bac798ff6cf14f90bce8d497d6084e608
zul03-cloud <malifzulkarnain2003@gmail.com>
2026-09-12 16:57:11 +0800

    feat: add product model and repository

---
+++ lib/data/repositories/models/product.dart
@@ -0,0 +1,31 @@
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final double rating;
+  final String thumbnail;
+  final List<String> images;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.rating,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0.0,
+      rating: (json['rating'] as num?)?.toDouble() ?? 0.0,
+      thumbnail: json['thumbnail'] ?? '',
+      images: json['images'] != null ? List<String>.from(json['images']) : [],
+    );
+  }
+}
